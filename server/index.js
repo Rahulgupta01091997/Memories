a91a4aa37a74fb047d1d2ff586541b87b30bf3af
Rahul Dilip Gupta <rahul.gupta1@thoughtworks.com>
2025-02-26 17:49:04 +0530

    Rahul | Memories project Backend config

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://rahulraina426:dZw8pe9EQge7bFWn@mern.bbaya.mongodb.net/?retryWrites=true&w=majority&appName=MERN";
+const PORT = process.env.PORT || 4200;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error));
