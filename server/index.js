4fb8facc5717a1cb1487eeed95e6996cd5e21b59
Rahul Dilip Gupta <rahul.gupta1@thoughtworks.com>
2025-03-03 16:15:29 +0530

    Rahul | Moved env variables to dotenv file

--- server/index.js
+++ server/index.js
@@ -2,22 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
-const CONNECTION_URL =
-  "mongodb+srv://rahulraina426:dZw8pe9EQge7bFWn@mern.bbaya.mongodb.net/?retryWrites=true&w=majority&appName=MERN";
 const PORT = process.env.PORT || 4200;
 
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
   )
